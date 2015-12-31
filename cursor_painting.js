eaf0f233c03d8da9df1d18120bac5bbe8a9d8424
Scott Todd <scott.todd0@gmail.com>
2015-12-31 15:21:05 -0800

    Cleanup.

--- cursor_painting.js
+++ cursor_painting.js
@@ -1,6 +1,14 @@
 var canvas = document.getElementById('cursor-canvas');
 var ctx = canvas.getContext('2d');
 
+var isFullscreen = function() {
+  return (
+      !!document.fullScreenElement ||
+      !!document.mozFullScreenElement ||
+      !!document.webkitFullscreenElement ||
+      !!document.msFullscreenElement);
+};
+
 var goFullscreen = function(element) {
   if (element.requestFullscreen) {
     element.requestFullscreen();
@@ -13,18 +21,27 @@ var goFullscreen = function(element) {
   }
 };
 
-var isFullscreen = function() {
-  return (
-      !!document.fullScreenElement ||
-      !!document.mozFullScreenElement ||
-      !!document.webkitFullscreenElement ||
-      !!document.msFullscreenElement);
-};
-
 var startButton = document.getElementById('start-button');
 startButton.addEventListener('click', function() {
   goFullscreen(canvas);
 });
+
+var enterFullscreen = function() {
+  // Resize canvas to match screen size.
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+
+  document.body.addEventListener('mousemove', mousemoveFunction);
+};
+
+var exitFullscreen = function() {
+  // Clear canvas :(
+  // TODO: properly translate/rescale canvas contents between modes?
+  canvas.width = 0;
+  canvas.height = 0;
+
+  document.body.removeEventListener('mousemove', mousemoveFunction);
+};
 
 var fullscreenEvents = [
     'webkitfullscreenchange',
@@ -33,22 +50,23 @@ var fullscreenEvents = [
     'MSFullscreenChange'];
 
 var fullscreenEventFunction = function(e) {
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-
-  var text = isFullscreen() ? 'Fullscreen' : 'Not fullscreen';
-  ctx.fillText(text, 10, 50);
+  if (isFullscreen()) {
+    enterFullscreen();
+  } else {
+    exitFullscreen();
+  }
 };
 
 for (var i = 0; i < fullscreenEvents.length; ++i) {
   document.body.addEventListener(fullscreenEvents[i], fullscreenEventFunction);
 }
 
-document.body.addEventListener('mousemove', function(e) {
+var mousemoveFunction = function(e) {
+  // Draw a dot where the mouse is.
   var x = e.clientX;
   var y = e.clientY;
 
   ctx.fillStyle = '#000000';
   ctx.rect(x, y, 1, 1);
   ctx.fill();
-});
+};
